55369268e129f3bcd2e14bc4ad85e9da076ef626
Zartdc <daniel.c.dan@gmail.com>
2021-09-03 15:53:54 -0300

    style header add icon

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { Container } from './styles';
 
+import GroupIcon from '@material-ui/icons/Group';
+
 export default function Header() {
   return (
     <Container>
+      <GroupIcon style={{ fontSize: 30, padding: 5 }}>Group</GroupIcon>    
       <h1>GroupCard</h1>
     </Container>
   );
--- src/component/Header/styles.js
+++ src/component/Header/styles.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 80px;
+
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap');
+
+  height: 70px;
   padding: 0 30px;
   background: #7159c1;
   color: #FFF;
   display: flex;
   align-items: center;
+
+  font: 14px 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased !important;
 `;
