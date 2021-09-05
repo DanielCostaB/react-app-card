dc31fb1b2a409803ba767a39830552348373cebd
Zartdc <daniel.c.dan@gmail.com>
2021-09-05 16:50:06 -0300

    Removendo entradas inválidas

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,14 +5,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyCOyFe_J9aMU8F4YXmGIMSw9z1Fw6R-9Wk",
-  authDomain: "app-card-d8729.firebaseapp.com",
-  databaseURL: "https://app-card-d8729-default-rtdb.firebaseio.com",
-  projectId: "app-card-d8729",
-  storageBucket: "app-card-d8729.appspot.com",
-  messagingSenderId: "1028969209417",
-  appId: "1:1028969209417:web:d26c6b9e7f4cc963ec2998"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
